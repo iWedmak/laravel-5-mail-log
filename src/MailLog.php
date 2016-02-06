299e618d968a4311ae42507274db752cf951f880
wedmak <wedmak@msk1.ethdns.com>
2016-02-06 19:24:37 +0300

    first commit

---
+++ src/MailLog.php
@@ -0,0 +1,88 @@
+<?php namespace iWedmak\MailLoger;
+
+class MailLoger
+{
+	
+    private $model;
+    
+    public function __construct()
+    {
+        $this->model=Config::get('maillog.model');
+    }
+    
+    public function onRead($id)
+	{
+		try
+		{
+			$mailLog=$this->model::where('id', $id)->firstOrFail();
+			$mailLog->read=true;
+			$mailLog->save();
+		}
+		catch(Exception $e)
+		{
+			//pre($e->getMessage());
+		}
+	}
+
+	public function onSend($message)
+	{
+		$body=$message->getBody();
+		$subject=$message->getSubject();
+        $to=key($message->getTo());
+        @$bcc=key($message->getBcc());
+        if(isset($bcc) && !empty($bcc) && ( $bcc==\Config::get('maillog.bcc') ))
+        {
+            
+        }
+        else
+        {
+			$mailLog=$this->model::where('to', $to)->where('subject', $subject)->where('body', strip_tags($body))->first();
+			if(isset($mailLog['id']) && !empty($mailLog['id']))
+			{
+				$message->setTo(array());
+                $mailLog->increment('attempt');
+                /*
+				$array=array(
+							'message'=>'Attempt to send email second time, prevented',
+							'to'=>$to,
+							'subject'=>$subject,
+							//'body'=>$body,
+						);
+				$mail=\Mail::send(array('html'=>'emails.data'), array('data'=>$array), function($message)
+				{
+					$message
+					->to('iwedmak@gmail.com')
+					->bcc('skeep@lavkalavka.com')
+					->subject('Попытка отправить емейл повторно')
+					;
+				});
+                //*/
+			}
+			else
+			{
+				$mailLog=new $this->model;
+				$mailLog->to=$to;
+				$mailLog->subject=$subject;
+				$mailLog->body=strip_tags($body);
+				$mailLog->save();
+				$body=$body.'<img src="'.\URL::route('MailRead', $mailLog['id']).'" height="1px" width="1px">';
+				$message->setBody($body);
+			}
+		}
+		$message->setBcc(array());
+        
+		//pre($subject);
+        //\App::abort(404);
+        //unset($message);
+        //$message=false;
+        //return unset($message);
+	}
+
+	public function subscribe($events)
+    {
+        $events->listen(
+            'App\Events\MessageSending',
+            'iWedmak\MailLoger\MailLoger@onSend'
+        );
+    }
+}
---
+++ src/commands/MigrationCommand.php
@@ -0,0 +1,73 @@
+<?php namespace iWedmak\MailLoger;
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Zizaco\Entrust
+ */
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
+class MigrationCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'maillog:migration';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a migration following the Maillog specifications.';
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $this->laravel->view->addNamespace('maillog', substr(__DIR__, 0, -8).'views');
+        $rolesTable          = Config::get('maillog.log_table');
+        $this->line('');
+        $this->info( "Table: $logTable" );
+        $message = "A migration that creates '$logTable'".
+        " table will be created in database/migrations directory";
+        $this->comment($message);
+        $this->line('');
+        if ($this->confirm("Proceed with the migration creation? [Yes|no]", "Yes")) {
+            $this->line('');
+            $this->info("Creating migration...");
+            if ($this->createMigration($logTable)) {
+                $this->info("Migration successfully created!");
+            } else {
+                $this->error(
+                    "Couldn't create migration.\n Check the write permissions".
+                    " within the database/migrations directory."
+                );
+            }
+            $this->line('');
+        }
+    }
+    /**
+     * Create the migration.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    protected function createMigration($logTable)
+    {
+        $migrationFile = base_path("/database/migrations")."/".date('Y_m_d_His')."_maillog_setup_tables.php";
+        $data = compact('logTable');
+        $output = $this->laravel->view->make('maillog::generators.migration')->with($data)->render();
+        if (!file_exists($migrationFile) && $fs = fopen($migrationFile, 'x')) {
+            fwrite($fs, $output);
+            fclose($fs);
+            return true;
+        }
+        return false;
+    }
+}
